13a49236200ce6109509cd0bb433b1b4a4c9cbed
cheildo <cheickzida@gmail.com>
2024-11-27 15:22:40 +0800

    configure hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,41 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-ignition-ethers");
+require("dotenv").config();
+
+const privateKey = process.env.PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.27",
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+
+  networks: {
+    sepolia: {
+      url: `https://sepolia.drpc.org`,
+      accounts: [privateKey],
+    },
+    BSC_Testnet: {
+      url: "https://bsc-testnet-dataseed.bnbchain.org",
+      accounts:[privateKey],
+    },
+    BSC_Mainnet: {
+      url: "https://bsc-dataseed.bnbchain.org",
+      accounts: [privateKey],
+    },
+  },
+
+  sourcify: {
+    enabled: false
+  },
+
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API,
+  },
 };
